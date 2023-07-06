a34a927113a076120344a329c630039eaca7c054
agakhanyantsartur <agakhanyantsartur@gmail.com>
2023-07-06 23:42:34 +0500

    Implemented search box with async movies loadings, final version of mini-app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
@@ -6,13 +7,16 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'https://www.omdbapi.com?apikey=7ffed242';
 
+
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -22,22 +26,32 @@ const App = () => {
     return (
         <div className="app">
             <h1>MovieLand</h1>
+
             <div className="search">
                 <input 
                     placeholder="Search for movies"
-                    value="Superman"
-                    onChange={() => {}}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                     src={SearchIcon}
                     alt="search" 
-                    onClick={() => {}}                   
+                    onClick={() => searchMovies(searchTerm)}                   
                 />
             </div>
 
-            <div className="container">
-
-            </div>
+            {movies?.length > 0 
+                ? (
+                    <div className="container">
+                         {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                )}
         </div>
     );
 }
